8ca53561b30a75aeb70dd1e52ae03f9bd7cf012f
Fabio Forni <fabio0x@keemail.me>
2018-11-16 00:18:33 +0100

    CLI: make scrape command work (still incomplete)

--- pykamino/_cli/commands/scrape.py
+++ pykamino/_cli/commands/scrape.py
@@ -1,11 +1,14 @@
 from pykamino._cli.commands import command
+from pykamino.scraper.scraper import Scraper
 
 class Scrape(command.Command):
     def __init__(self, name=None, description=None):
         super().__init__(name, description)
 
     def __call__(self, args):
-        print('Wow this is a daemon! Well, not quite.')
+        if 'start' in args:
+            scraper = Scraper()
+            scraper.start()
 
     def set_arguments(self, parser):
-        pass
+        parser.add_argument('start')
