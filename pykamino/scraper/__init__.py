f6d81aaa1831db109a549963f72d59268da74bcd
Fabio Forni <fabio0x@keemail.me>
2018-11-27 10:57:41 +0100

    Store matches in the database

--- pykamino/scraper/__init__.py
+++ pykamino/scraper/__init__.py
@@ -1,22 +1,28 @@
+from pykamino.db.models import Trade
 import cbpro
 
 
 class Scraper(cbpro.WebsocketClient):
-    CACHE_SIZE = 1000
+    CACHE_SIZE = 100
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(channels=['full'], *args, **kwargs)
+    def __init__(self, db_conn, *args, **kwargs):
+        super().__init__(channels=['matches'], *args, **kwargs)
+        self.db_conn = db_conn
 
     def on_open(self):
         self.messages = []
 
     def on_message(self, msg):
         if len(self.messages) == Scraper.CACHE_SIZE:
-            # TODO: parse things here
+            Trade.insert_many(list(self.matches(self.messages)))
             self.messages.clear()
         else:
             self.messages.append(msg)
+        print(msg)
 
     def on_close(self):
         pass
         # TODO: parse the remaining data in the list, then empty it
+
+    def matches(self, msg_list):
+        return (msg for msg in msg_list if msg['type'] == 'last_match' or msg['type'] == 'match')
