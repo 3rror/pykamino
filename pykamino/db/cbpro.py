b657c6adbe2306386f6ad161de2986819a80e036
Fabio Forni <fabio0x@keemail.me>
2018-12-17 20:43:05 +0100

    Fix msg_to_order

--- pykamino/db/cbpro.py
+++ pykamino/db/cbpro.py
@@ -17,11 +17,14 @@ def book_snapshot_to_orders(snap, product):
 
 def msg_to_order(msg) -> (Order, OrderTimeline):
     """
-    Convert a Coinbase message into an `OrderTimeline` instance, and the related `Order` instance if any.
+    Convert a Coinbase message into an `OrderTimeline` instance, and the related `Order` instance.
+
+    If the message is related to a market order, meaning the order has never been
+    on the book, both variables will be `None`.
 
     Returns:
         order: an Order instance if available, else None
-        timeline: an OrderTimeline instance
+        timeline: an OrderTimeline instance if message is not a market order, else None
     Raises:
         ValueError: if message type is not 'change', 'done' or 'open'.
     """
@@ -30,14 +33,16 @@ def msg_to_order(msg) -> (Order, OrderTimeline):
     order = None
     if msg['type'] == 'open':
         order = Order(id=msg['order_id'], side=msg['side'], product=msg['product_id'])
-    try: reason = msg['reason']
-    # reason can be null
-    except KeyError: reason = None
-    timeline = OrderTimeline(remaining_size=msg['remaining_size'],
-                             price=msg['price'],
-                             time=msg['time'],
-                             order=order if order is not None else msg['order_id'],
-                             reason=reason)
+    try:
+        timeline = OrderTimeline(remaining_size=msg['remaining_size'],
+                                price=msg['price'],
+                                time=msg['time'],
+                                order=order if order is not None else msg['order_id'],
+                                reason=msg.get('reason', None))
+    except KeyError:
+        # from Coinbase: market orders will not have a remaining_size
+        # or price field as they are never on the open order book at a given price.
+        timeline = None
     return order, timeline
 
 
@@ -52,8 +57,7 @@ def msg_to_trade(msg) -> Trade:
     """
     if msg['type'] != 'match':
         raise ValueError("Message type is not 'match'")
-    trade = Trade(id=msg['trade_id'],
-                  side=msg['side'],
+    trade = Trade(side=msg['side'],
                   size=msg['size'],
                   product=msg['product_id'],
                   price=msg['price'],
