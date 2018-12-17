c14c677b5cd838faea82900df30a008a9f48aaa1
Fabio Forni <fabio0x@keemail.me>
2018-12-17 14:08:59 +0100

    Better code organization for db/cbpro.py

--- pykamino/db/cbpro.py
+++ pykamino/db/cbpro.py
@@ -2,38 +2,60 @@
 Adapt Coinbase Pro's data structure to our database Models
 """
 
-from playhouse.shortcuts import dict_to_model
-from pykamino.db import Order, OrderTimeline
+from pykamino.db import Order, OrderTimeline, Trade
 
 
 def book_snapshot_to_orders(snap, product):
-    orders = []
-    timelines = []
     for key in (k for k in snap if k in ['bids', 'asks']):
         for item in snap[key]:
+            # snap[0]: price; snap[1]: size; snap[2]: uuid
             # Remove the leading 's' (for plural) from each 'side'
-            orders.append(Order(id=item[2], side=key[:-1], product=product))
-            timelines.append(OrderTimeline(price=item[0],
-                                           remaining_size=item[1],
-                                           order=orders[-1]))
-    return orders, timelines
+            order = Order(id=item[2], side=key[:-1], product=product)
+            timeline = OrderTimeline(price=item[0], remaining_size=item[1], order=order)
+            yield (order, timeline)
 
 
-def dict_to_orders(orders):
+def msg_to_order(msg) -> (Order, OrderTimeline):
     """
-    Return two lists: the former is a list of pure `Order` instances, the latter
-    is a list of `OrderTimeline` objects.
-    """
-    orders_to_save = []
-    timelines = []
+    Convert a Coinbase message into an `OrderTimeline` instance, and the related `Order` instance if any.
 
-    def dict_to_timeline(order): timelines.append(
-        dict_to_model(OrderTimeline, order, ignore_unknown=True))
-    for order in orders:
-        if order['type'] in ['change', 'done']:
-            dict_to_timeline(order)
-        elif order['type'] == 'open':
-            orders_to_save.append(dict_to_model(
-                Order, order, ignore_unknown=True))
-            dict_to_timeline(order)
-    return orders_to_save, timelines
+    Returns:
+        order: an Order instance if available, else None
+        timeline: an OrderTimeline instance
+    Raises:
+        ValueError: if message type is not 'change', 'done' or 'open'.
+    """
+    if msg['type'] not in ['change', 'done', 'open']:
+        raise ValueError("Message type is not 'open', 'change', or 'done'")
+    order = None
+    if msg['type'] == 'open':
+        order = Order(id=msg['order_id'], side=msg['side'], product=msg['product_id'])
+    try: reason = msg['reason']
+    # reason can be null
+    except KeyError: reason = None
+    timeline = OrderTimeline(remaining_size=msg['remaining_size'],
+                             price=msg['price'],
+                             time=msg['time'],
+                             order=order if order is not None else msg['order_id'],
+                             reason=reason)
+    return order, timeline
+
+
+def msg_to_trade(msg) -> Trade:
+    """
+    Convert a Coinbase message into an `Trade` instance.
+
+    Returns:
+        trade: a Trade instance.
+    Raises:
+        ValueError: if message type is not 'match'.
+    """
+    if msg['type'] != 'match':
+        raise ValueError("Message type is not 'match'")
+    trade = Trade(id=msg['trade_id'],
+                  side=msg['side'],
+                  size=msg['size'],
+                  product=msg['product_id'],
+                  price=msg['price'],
+                  time=msg['time'])
+    return trade
