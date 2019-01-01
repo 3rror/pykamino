0c4e25e8986d6b368f01c3a230ef7ba815808153
Fabio Forni <fabio0x@keemail.me>
2019-01-01 17:06:15 +0100

    Rework scraper logic with threads

--- pykamino/scraper/__init__.py
+++ pykamino/scraper/__init__.py
@@ -1,120 +1,133 @@
+from datetime import datetime
 import itertools
 import cbpro
-import threading
-from peewee import ProgrammingError
+from peewee import ProgrammingError, Case
+from queue import Queue
+from threading import Thread, Condition, Event
 
-from pykamino.db import Order, OrderTimeline, Trade, database
-from pykamino.db.cbpro import FullBookSnapshot, msg_to_order, msg_to_trade
-
-# This module uses the Observer pattern
-
-
-msg_lock = threading.RLock()
+from pykamino.db import Order, OrderHistory as History, Trade, database
+from pykamino.db.cbpro import Snapshot, msg_to_order_dict, msg_to_history_dict, msg_to_trade_dict
 
 
 class Scraper():
     def __init__(self, products=['BTC-USD']):
-        self.receiver = Receiver(products=products)
-        self.query_client = cbpro.PublicClient()
-        self.products = products
+        self._seqs = {prod: -1 for prod in products}
+        self._receiver = Receiver(products=products)
+        self._filter = Filter(sequences=self._seqs)
+        self._storer = MessageStorer()
 
-    def start(self):
-        snap = FullBookSnapshot(products=self.products)
-        self.receiver.start()
+    @property
+    def products(self):
+        return self._seqs.keys()
+
+    def save_snapshot(self, product):
+        snap = Snapshot(product)
         seq = snap.download()
         snap.insert()
+        return seq
 
-        def is_newer(msg):
-            try:
-                msg['sequence'] > seq
-            except KeyError:
-                return False
+    def start(self):
+        # TODO: don't allow to start an already-started Scraper
+        self._receiver.start()
+        for p in self.products:
+            # _seqs is a mutable object so it's passed by reference.
+            # We don't need to pass it again to _filter
+            self._seqs[p] = self.save_snapshot(p)
+        self._filter.start()
+        self._storer.start()
 
-        with msg_lock:
-            self.receiver.messages = filter(is_newer, self.receiver.messages)
-        self.receiver.subscribe(self.save_messages)
-
-
-    def save_book_snapshot(self):
-        orders = []
-        timelines = []
-        for prod in self.products:
-            book_snap = self.query_client.get_product_order_book(prod, level=3)
-            new_orders, new_timelines = zip(*book_snapshot_to_orders(book_snap, prod))
-            orders.extend(new_orders)
-            timelines.extend(new_timelines)
-        # Don't insert already existing orders
-        query = Order.select(Order.id).where(Order.id.in_([el.id for el in orders])).execute()
-        filt = set((str(order.id) for order in query))
-        orders = set(filter(lambda x: str(x.id) not in filt, orders))
-        timelines = filter(lambda x: x.order in orders, timelines)
-        with database.atomic():
-            Order.bulk_create(orders)
-            OrderTimeline.bulk_create(timelines)
-        return book_snap['sequence']
-
-    def save_messages(self, *args, **kwargs):
-        with msg_lock:
-            order_msgs, trade_msgs = self.classify_messages()
-            orders, timelines = zip(*(msg_to_order(msg) for msg in order_msgs if msg['type'] != 'received'))
-            orders = list(filter(lambda x: x is not None, orders))
-            timelines = list(filter(lambda x: x is not None, timelines))
-
-            query_filt = Order.select(Order.id).where(Order.id.in_([t.order_id for t in timelines])).execute()
-            filt = set((str(r.id) for r in query_filt)).union(set((str(o.id) for o in orders)))
-            #timelines = list(filter(lambda x: x.order_id in filt, timelines))
-            print(timelines[0].order_id)
-            import pdb
-            pdb.set_trace()
-            with database.atomic():
-                #try:
-                #    Trade.bulk_create((msg_to_trade(msg) for msg in trade_msgs))
-                #except ProgrammingError:
-                #    pass
-                with database.atomic():
-                    Order.bulk_create(orders)
-                    #OrderTimeline.bulk_create(timelines)
-            self.receiver.messages.clear()
+    def stop(self):
+        self._receiver.stop()
+        self._filter.stop()
+        self._storer.stop()
 
 
-    def classify_messages(self):
-        """
-        Split the list of messages in two iterators: orders and trades
-        """
-        orders, trades = itertools.tee(self.receiver.messages)
+### Threading stuff ###
 
-        def cond(msg): return msg['type'] == 'match'
-        return itertools.filterfalse(cond, orders), filter(cond, trades)
+class GracefulThread(Thread):
+    def __init__(self):
+        super().__init__()
+        self._close_cond = Event()
+
+    def run(self):
+         while not self._close_cond.is_set():
+             self.task()
+    
+    def task(self):
+        raise NotImplementedError()
+
+    def stop(self):
+        self._close_cond.set()
+
+
+msg_queue = Queue()
+filtered_msgs = []
+filt_msgs_lock = Condition()
 
 
 class Receiver(cbpro.WebsocketClient):
-    def __init__(self, buffer_size=100, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(channels=['full'], **kwargs)
-        self.buffer_size = buffer_size
-        self.observers = []
 
     def on_open(self):
-        self._messages = []
+        pass
 
     def on_message(self, msg):
-        with msg_lock:
-            self._messages.append(msg)
-            msg_count = len(self._messages)
-        if msg_count % self.buffer_size == 0:
-            self.fire(msg_count=msg_count)
+        msg_queue.put(msg)
 
-    def subscribe(self, callback):
-        self.observers.append(callback)
-        return callback
 
-    def fire(self, *args, **kwargs):
-        for callback in self.observers:
-            callback(*args, **kwargs)
+class Filter(GracefulThread):
+    def __init__(self, sequences=None):
+        super().__init__()
+        self._seqs = sequences
+
+    def task(self):
+        msg = msg_queue.get()
+        msg_queue.task_done()
+        try:
+            if msg['sequence'] > self._seqs[msg['product_id']]:
+                with filt_msgs_lock:
+                    filtered_msgs.append(msg)
+                    # TODO: make this magic number less magic
+                    if len(filtered_msgs) >= 200:
+                        filt_msgs_lock.notify_all()
+        except KeyError:
+            # The message received is probably the header message.
+            # Safe to ignore.
+            pass
+
+
+class MessageStorer(GracefulThread):
+    def task(self):
+        with filt_msgs_lock:
+            filt_msgs_lock.wait()
+            trades = filter(lambda x: x['type']=='match', filtered_msgs)
+            orders = filter(lambda x: x['type'] not in ['activate', 'match', 'received'], filtered_msgs)
+            self.store_messages(orders, trades)
+            filtered_msgs.clear()
+
+    def store_messages(self, orders, trades):
+        new_ord, hist, to_close = self.classify_orders(orders)
+        trades = [msg_to_trade_dict(t) for t in trades]
+        updates = Case(Order.id, [(o['id'], datetime.strptime(o['close_time'], '%Y-%m-%dT%H:%M:%S.%fZ')) for o in to_close])
+        with database.atomic():
+            if trades:
+                Trade.insert_many(trades).execute()
+            Order.insert_many(new_ord).execute()
+            History.insert_many(hist).execute()
+            with database.atomic():
+                Order.update(close_time=updates).where(Order.id.in_([o['id'] for o in to_close])).execute()
     
-    @property
-    def messages(self):
-        return self._messages
-    
-    @messages.setter
-    def messages(self, msgs):
-        self._messages = msgs if isinstance(msgs, list) else list(msgs)
+    def classify_orders(self, orders):
+        new_orders = []
+        history = []
+        to_close = []
+        for o in orders:
+            if o['type'] == 'done':
+                to_close.append({'id': o['order_id'], 'close_time': o['time']})
+            elif o['type'] == 'change':
+                history.append(msg_to_history_dict(o))
+            else:
+                new_orders.append(msg_to_order_dict(o))
+                history.append(msg_to_history_dict(o))
+        return new_orders, history, to_close
