66435d9369511679f3fb8504617cff59cb8fcfdc
Fabio Forni <public@redaril.me>
2019-02-02 19:44:35 +0100

    Add some trade feature tests

--- tests/features.py
+++ tests/features.py
@@ -1,4 +1,10 @@
 import unittest
+from datetime import datetime, timedelta
+
+from pykamino.db import Dbms, Trade, db_factory
+from pykamino.features.trades import TradesDataFrame
+
+db = db_factory(Dbms.SQLITE, ':memory:')
 
 
 class OrderFeatures(unittest.TestCase):
@@ -6,7 +12,28 @@ class OrderFeatures(unittest.TestCase):
 
 
 class TradeFeatures(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        data = []
+        dt = datetime(2010, 1, 30, 11, 00)
+        for i in range(0, 20):
+            data.append((i + 1,
+                         'sell' if i < 10 else 'buy',
+                         0.1 + 0.1 * i,
+                         'BTC-USD',
+                         1500 + 500 * i,
+                         dt + timedelta(minutes=10 * i)))
+        Trade.insert_many(data, fields=Trade._meta.fields).execute()
+        cls.dataframe = TradesDataFrame(list(Trade.select().dicts()))
+
+    def test_mean_price(self):
+        self.assertEqual(self.dataframe.mean_price(), 6250)
+    
+    def test_std_price(self):
+        self.assertAlmostEqual(self.dataframe.std_price(), 	2958.03989154, places=7)
+    
+    def test_buy_count(self):
+        self.assertEqual(self.dataframe.buy_count(), 10)
 
 
 if __name__ == '__main__':
