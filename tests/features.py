1819f88d360a4f288242108d7b9011e45c533eb3
Fabio Forni <public@redaril.me>
2019-02-03 14:54:58 +0100

    Populate tables for order feature test

--- tests/features.py
+++ tests/features.py
@@ -1,14 +1,42 @@
 import unittest
 from datetime import datetime, timedelta
 
-from pykamino.db import Dbms, Trade, db_factory
-from pykamino.features.trades import TradesDataFrame
+from pykamino.db import Dbms, Order, OrderHistory, Trade, db_factory
+from pykamino.features.trades import TradesDataFrame, extract
 
 db = db_factory(Dbms.SQLITE, ':memory:')
 
 
+def uuid_builder(i):
+    num = '{:032x}'.format(i)
+    return '{}-{}-{}-{}'.format(num[0:8], num[8:12], num[12:16], num[16:])
+
+
 class OrderFeatures(unittest.TestCase):
-    pass
+    @classmethod
+    def setUpClass(cls):
+        orders = []
+        hist = []
+        dt = datetime(2010, 1, 30, 10, 00)
+        for i in range(0, 20):
+            orders.append((uuid_builder(i),
+                           'bid' if i % 2 == 0 else 'ask',
+                           'BTC-USD',
+                           1500 + 500 * i,
+                           dt + timedelta(hours=5, minutes=i) if i % 3 == 0 else None))
+            hist.append((i+1,
+                         0.1 + 0.1 * i,
+                         dt + timedelta(minutes=i),
+                         uuid_builder(i)))
+        Order.insert_many(orders, fields=Order._meta.fields).execute()
+        OrderHistory.insert_many(hist, OrderHistory._meta.fields).execute()
+        OrderHistory.insert({'id': 100, 'amount': 100, 'time': dt + timedelta(hours=3),
+                             'order_id': uuid_builder(9)}).execute()
+        OrderHistory.insert({'id': 200, 'amount': 100, 'time': dt + timedelta(hours=3),
+                             'order_id': uuid_builder(11)}).execute()
+
+    def test_best_ask_order(self):
+        self.assertTrue(True)
 
 
 class TradeFeatures(unittest.TestCase):
@@ -47,6 +75,8 @@ class TradeFeatures(unittest.TestCase):
 
     def test_price_movement(self):
         self.assertEqual(self.dataframe.price_movement(), -9500)
+
+    # TODO: test CSV generation, not only calculations
 
 
 if __name__ == '__main__':
