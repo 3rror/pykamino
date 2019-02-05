f69790b24bc08fe24f1cc9750cff34e43d48e996
Fabio Forni <public@redaril.me>
2019-02-05 01:00:02 +0100

    Recreate DB at every test

--- tests/features.py
+++ tests/features.py
@@ -1,11 +1,12 @@
 import unittest
+from abc import abstractmethod
 from datetime import datetime, timedelta
 from decimal import Decimal
 
+from peewee import SqliteDatabase
 from pykamino.db import Dbms, Order, OrderHistory, Trade, db_factory
-from pykamino.features.trades import TradesDataFrame, extract
-
-db = db_factory(Dbms.SQLITE, ':memory:')
+from pykamino.features.trades import (TradesDataFrame, extract,
+                                      trades_in_time_window)
 
 
 def uuid_builder(i):
@@ -13,9 +14,35 @@ def uuid_builder(i):
     return '{}-{}-{}-{}'.format(num[0:8], num[8:12], num[12:16], num[16:])
 
 
-class OrderFeatures(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db = SqliteDatabase(':memory:')
+        self.db.bind(self.models)
+        self.db.connect()
+        self.db.create_tables(self.models)
+        self.populate_tables()
+
+    def tearDown(self):
+        self.db.drop_tables(self.models)
+        self.db.close()
+
+    @property
+    @abstractmethod
+    def models(self):
+        pass
+
+    @abstractmethod
+    def populate_tables(self):
+        pass
+
+
+class OrderFeatures(BaseTestCase):
+
+    @property
+    def models(self):
+        return [Order, OrderHistory]
+
+    def populate_tables(self):
         orders = []
         hist = []
         dt = datetime(2010, 1, 30, 10, 00)
@@ -40,20 +67,29 @@ class OrderFeatures(unittest.TestCase):
         self.assertTrue(True)
 
 
-class TradeFeatures(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+class TradeFeatures(BaseTestCase):
+
+    START_DT = datetime(2010, 1, 30, 11, 00)
+
+    @property
+    def models(self):
+        return [Trade]
+
+    def populate_tables(self):
         data = []
-        dt = datetime(2010, 1, 30, 11, 00)
         for i in range(0, 20):
             data.append((i + 1,
                          'sell' if i < 10 else 'buy',
                          Decimal('0.1') * (i+1),
                          'BTC-USD',
                          1500 + 500 * i,
-                         dt + timedelta(minutes=10 * i)))
+                         self.START_DT + timedelta(minutes=10 * i)))
         Trade.insert_many(data, fields=Trade._meta.fields).execute()
-        cls.dataframe = TradesDataFrame(list(Trade.select().dicts()))
+
+    def setUp(self):
+        super().setUp()
+        self.dataframe = trades_in_time_window(
+            self.START_DT, self.START_DT + timedelta(weeks=1), products=['BTC-USD'])
 
     def test_mean_price(self):
         self.assertEqual(self.dataframe.price_mean(), 6250)
