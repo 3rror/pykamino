a55bb795027269dfe939ea5fd31d667265f61bd9
Fabio Forni <public@redaril.me>
2019-02-08 18:51:40 +0100

    Use functional paradigm for trade features

--- pykamino/features/trades.py
+++ pykamino/features/trades.py
@@ -4,80 +4,89 @@ from itertools import tee
 
 import pandas
 from pykamino.db import Trade
+from pykamino.features.decorators import rounded
 
 
-class TradesDataFrame(pandas.DataFrame):
-
-    ROUND_DIGITS = 8
-
-    def trades_between_instants(self, start_ts, end_ts):
-        return self[self.time.between(start_ts, end_ts)]
-
-    def buys(self):
-        """Trades of type 'buy'."""
-        return self[self.side == "buy"]
-
-    def sells(self):
-        """Trades of type 'sell'."""
-        return self[self.side == "sell"]
-
-    def price_mean(self):
-        """Mean price."""
-        return round(self.price.mean(), self.ROUND_DIGITS)
-
-    def price_std(self):
-        """Standard deviation of prices."""
-        return round(self.price.astype(float).std(), self.ROUND_DIGITS)
-
-    def buy_count(self):
-        """Number of 'buy' trades."""
-        return len(self.buys())
-
-    def sell_count(self):
-        """Number of 'sell' trades."""
-        return len(self.sells())
-
-    def total_buy_volume(self):
-        """Total amount bought."""
-        return round(self.buys().amount.sum(), self.ROUND_DIGITS)
-
-    def total_sell_volume(self):
-        """Total amount sold."""
-        return round(self.sells().amount.sum(), self.ROUND_DIGITS)
-
-    def price_movement(self):
-        """Difference between the oldest and the most recent price."""
-        if len(self) == 0:
-            return None
-        if len(self) == 1:
-            return 0
-        first_trade = self.loc[self.time.idxmin()]
-        last_trade = self.loc[self.time.idxmax()]
-        return round(first_trade.price - last_trade.price, self.ROUND_DIGITS)
-
-    def compute_all(self):
-        return {
-            "buy_count": self.buy_count(),
-            "sell_count": self.sell_count(),
-            "total_buy_volume": self.total_buy_volume(),
-            "total_sell_volume": self.total_sell_volume(),
-            "price_mean": self.price_mean(),
-            "price_std": self.price_std(),
-            "price_movement": self.price_movement(),
-        }
+def buys(df):
+    """Trades of type 'buy'."""
+    return df[df.side == "buy"]
 
 
-def _extract_instant_features(trades, instant, next_instant):
-    trades_slice = trades.trades_between_instants(instant, next_instant)
-    features = trades_slice.compute_all()
+def sells(df):
+    """Trades of type 'sell'."""
+    return df[df.side == "sell"]
+
+
+@rounded
+def price_mean(df):
+    return df.price.mean()
+
+
+@rounded
+def price_std(df):
+    """Standard deviation of prices."""
+    return df.price.astype(float).std()
+
+
+def buy_count(df):
+    """Number of 'buy' trades."""
+    return len(buys(df))
+
+
+def sell_count(df):
+    """Number of 'sell' trades."""
+    return len(sells(df))
+
+
+@rounded
+def total_buy_volume(df):
+    """Total amount bought."""
+    return buys(df).amount.sum()
+
+
+@rounded
+def total_sell_volume(df):
+    """Total amount sold."""
+    return sells(df).amount.sum()
+
+
+@rounded
+def price_movement(df):
+    """Difference between the oldest and the most recent price."""
+    if len(df) == 0:
+        return None
+    if len(df) == 1:
+        return 0
+    first_trade = df.loc[df.time.idxmin()]
+    last_trade = df.loc[df.time.idxmax()]
+    return first_trade.price - last_trade.price
+
+
+# TODO: make the collection automatic
+def compute_all(df):
+    return {
+        "buy_count": buy_count(df),
+        "sell_count": sell_count(df),
+        "total_buy_volume": total_buy_volume(df),
+        "total_sell_volume": total_sell_volume(df),
+        "price_mean": price_mean(df),
+        "price_std": price_std(df),
+        "price_movement": price_movement(df),
+    }
+
+
+def select_trades(start_dt, end_dt, products):
+    query = (Trade.select()
+             .where(Trade.time.between(start_dt, end_dt) &
+                    Trade.product.in_(products)))
+    return pandas.DataFrame(list(query.dicts()))
+
+
+def features_from_subset(df, instant, next_instant):
+    trades_slice = df[df.time.between(instant, next_instant)]
+    features = compute_all(trades_slice)
     features['time'] = instant
     return features
-
-
-def trades_in_time_window(start_dt, end_dt, products):
-    query = Trade.select().where(Trade.time.between(
-        start_dt, end_dt) & Trade.product.in_(products))
-    return TradesDataFrame(list(query.dicts()))
 
 
 def extract(start_dt, end_dt, resolution='1min', products=['BTC-USD']):
@@ -87,11 +96,10 @@ def extract(start_dt, end_dt, resolution='1min', products=['BTC-USD']):
         next(b, None)
         return zip(a, b)
 
-    trades = trades_in_time_window(start_dt, end_dt, products)
-    instants = instants = pandas.date_range(
-        start=start_dt, end=end_dt, freq=resolution).tolist()
+    trades = select_trades(start_dt, end_dt, products)
+    windows = pandas.date_range(start=start_dt, end=end_dt,
+                                freq=resolution).tolist()
     with multiprocessing.Pool() as pool:
-        params = [(trades, instant, next_instant)
-                  for instant, next_instant in pairwise(instants)]
-        features = pool.starmap(_extract_instant_features, params)
+        params = [(trades, start, end) for start, end in pairwise(windows)]
+        features = pool.starmap(features_from_subset, params)
     return features
--- tests/features.py
+++ tests/features.py
@@ -1,3 +1,4 @@
+from pykamino.features.trades import *
 import unittest
 from abc import abstractmethod
 from datetime import datetime, timedelta
@@ -5,8 +6,6 @@ from decimal import Decimal
 
 from peewee import SqliteDatabase
 from pykamino.db import Dbms, Order, OrderHistory, Trade, db_factory
-from pykamino.features.trades import (TradesDataFrame, extract,
-                                      trades_in_time_window)
 
 
 def uuid_builder(i):
@@ -38,6 +37,8 @@ class BaseTestCase(unittest.TestCase):
 
 class OrderFeatures(BaseTestCase):
 
+    START_DT = datetime(2010, 1, 30, 11, 00)
+
     @property
     def models(self):
         return [Order, OrderHistory]
@@ -45,30 +46,32 @@ class OrderFeatures(BaseTestCase):
     def populate_tables(self):
         orders = []
         hist = []
-        dt = datetime(2010, 1, 30, 10, 00)
         for i in range(0, 20):
             orders.append((uuid_builder(i),
                            'bid' if i % 2 == 0 else 'ask',
                            'BTC-USD',
                            1500 + 500 * i,
-                           dt + timedelta(hours=5, minutes=i) if i % 3 == 0 else None))
+                           self.START_DT + timedelta(hours=5, minutes=i) if i % 3 == 0 else None))
             hist.append((i+1,
-                         0.1 + 0.1 * i,
-                         dt + timedelta(minutes=i),
+                         Decimal('0.1') * (i+1),
+                         self.START_DT + timedelta(minutes=i),
                          uuid_builder(i)))
         Order.insert_many(orders, fields=Order._meta.fields).execute()
         OrderHistory.insert_many(hist, OrderHistory._meta.fields).execute()
-        OrderHistory.insert({'id': 100, 'amount': 100, 'time': dt + timedelta(hours=3),
+        OrderHistory.insert({'id': 100, 'amount': 100, 'time': self.START_DT + timedelta(hours=3),
                              'order_id': uuid_builder(9)}).execute()
-        OrderHistory.insert({'id': 200, 'amount': 100, 'time': dt + timedelta(hours=3),
+        OrderHistory.insert({'id': 200, 'amount': 100, 'time': self.START_DT + timedelta(hours=3),
                              'order_id': uuid_builder(11)}).execute()
+
+    def setUp(self):
+        super().setUp()
+        # self.dataframe =
 
     def test_best_ask_order(self):
         self.assertTrue(True)
 
 
 class TradeFeatures(BaseTestCase):
-
     START_DT = datetime(2010, 1, 30, 11, 00)
 
     @property
@@ -88,36 +91,37 @@ class TradeFeatures(BaseTestCase):
 
     def setUp(self):
         super().setUp()
-        self.dataframe = trades_in_time_window(
+        self.dataframe = select_trades(
             self.START_DT, self.START_DT + timedelta(weeks=1), products=['BTC-USD'])
 
     def test_mean_price(self):
-        self.assertEqual(self.dataframe.price_mean(), 6250)
+        self.assertEqual(price_mean(self.dataframe), 6250)
 
     def test_std_price(self):
-        self.assertAlmostEqual(self.dataframe.price_std(),
+        self.assertAlmostEqual(price_std(self.dataframe),
                                2958.03989154, delta=1e-8)
 
     def test_buy_count(self):
-        self.assertEqual(self.dataframe.buy_count(), 10)
+        self.assertEqual(buy_count(self.dataframe), 10)
 
     def test_sell_count(self):
-        self.assertEqual(self.dataframe.sell_count(), 10)
+        self.assertEqual(sell_count(self.dataframe), 10)
 
     def test_total_buy_volume(self):
-        self.assertEqual(self.dataframe.total_buy_volume(), 15.5)
+        self.assertEqual(total_buy_volume(self.dataframe), 15.5)
 
     def test_total_sell_volume(self):
-        self.assertEqual(self.dataframe.total_sell_volume(), 5.5)
+        self.assertEqual(total_sell_volume(self.dataframe), 5.5)
 
     def test_price_movement(self):
-        self.assertEqual(self.dataframe.price_movement(), -9500)
+        self.assertEqual(price_movement(self.dataframe), -9500)
 
-    def test_extract_subset(self):
-        self.assertEqual(
-            self.dataframe.trades_between_instants(
-                datetime(2010, 1, 30, 11, 00), datetime(2010, 1, 30, 11, 46)).id.tolist(),
-            list(range(1, 6)))
+    def test_features_from_subset(self):
+        # Pandas can be very unintuitive. Let's test if we can get
+        # features from a subset of the dataframe as well...
+        subset = features_from_subset(self.dataframe,
+                                      self.START_DT, self.START_DT + timedelta(minutes=46))
+        self.assertEqual(subset['price_mean'], 2500)
 
     # TODO: test CSV generation, not only calculations
 
