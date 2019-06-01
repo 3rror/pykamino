8647e60a269bd2809541615b641c20b15f9ae2c6
Fabio Forni <fabio@redaril.me>
2019-06-01 18:35:58 +0200

    Improve egg generation

--- pykamino/_cli/config.py
+++ pykamino/_cli/config.py
@@ -1,8 +1,15 @@
+import os
+from shutil import copy
 from os import path
 
+import toml
 from appdirs import user_config_dir
 
-import toml
+filename = 'pykamino.toml'
+user_path = path.join(user_config_dir('pykamino'), filename)
+
+if not path.exists(user_path):
+    orig_file = copy(path.join(path.dirname(__file__), filename), user_path)
 
 # At the moment, this is just a dict. In future, it may become a fancy class
-config = toml.load(path.join(user_config_dir('pykamino'), 'pykamino.toml'))
+config = toml.load(user_path)
--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
-from appdirs import user_config_dir
 from setuptools import find_namespace_packages, setup
+
+import pykamino
 
 with open('requirements.txt', 'r') as req:
     requirements = req.read().splitlines()
 
 setup(
-    name='pykamino',
-    version='0.9.0',
+    name=pykamino.__name__,
+    version=pykamino.__version__,
     description='All-In-One tool for project Naboo',
     url='https://gitlab-rbl.unipv.it/fforni/pykamino',
     author='Fabio Forni, Gianluca Andreotti',
     author_email='fabio.forni01@universitadipavia.it, gianluca.andreotti01@universitadipavia.it',
     license='Apache 2',
-    # This is a namespace project as it lacks of __init__.py files
-    packages=find_namespace_packages(),
+    package_dir={'': 'pykamino'},
+    packages=find_namespace_packages('pykamino'),
     zip_safe=True,
     test_suite='tests',
     scripts=['bin/pykamino'],
-    data_files=[(user_config_dir('pykamino'), [
-                 'pykamino/data/pykamino.toml'])],
-    install_requires=requirements
+    python_requires='>=3.6.0',
+    install_requires=requirements,
+    extras_require={
+        'postgresql': ['psycopg2~=2.8.0']
+    },
 )
