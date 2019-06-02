2818fb95660c4af26e4765655857e197cfcafced
Fabio Forni <fabio@redaril.me>
2019-06-02 16:29:37 +0200

    It's makedirs(), not mkdir()...

--- pykamino/_cli/config.py
+++ pykamino/_cli/config.py
@@ -1,6 +1,6 @@
 import os
-from shutil import copy
 from os import path
+from shutil import copy
 
 import toml
 from appdirs import user_config_dir
@@ -9,7 +9,7 @@ filename = 'pykamino.toml'
 user_path = path.join(user_config_dir('pykamino'), filename)
 
 if not path.exists(user_path):
-    os.mkdir(user_config_dir('pykamino'))
+    os.makedirs(user_config_dir('pykamino'), exist_ok=True)
     orig_file = copy(path.join(path.dirname(__file__), filename), user_path)
 
 # At the moment, this is just a dict. In future, it may become a fancy class
