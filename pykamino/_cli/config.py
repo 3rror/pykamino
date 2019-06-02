29a1d4932ce91e5ff9fd72e6beaffc203e957e59
Fabio Forni <fabio@redaril.me>
2019-06-02 15:51:04 +0200

    Create user's config dir if nonexistent

--- pykamino/_cli/config.py
+++ pykamino/_cli/config.py
@@ -9,6 +9,7 @@ filename = 'pykamino.toml'
 user_path = path.join(user_config_dir('pykamino'), filename)
 
 if not path.exists(user_path):
+    os.mkdir(user_config_dir('pykamino'))
     orig_file = copy(path.join(path.dirname(__file__), filename), user_path)
 
 # At the moment, this is just a dict. In future, it may become a fancy class
