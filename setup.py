7791f1f9ced42d26d7373e097bd366278760e1e3
Fabio Forni <fabio@redaril.me>
2019-06-02 15:11:58 +0200

    setup.py: ensure to install config files as well

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email='fabio.forni01@universitadipavia.it, gianluca.andreotti01@universitadipavia.it',
     license='Apache 2',
     packages=find_packages(),
+    include_package_data=True,
     zip_safe=True,
     test_suite='tests',
     scripts=['bin/pykamino'],
