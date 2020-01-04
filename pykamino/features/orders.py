c882491f9265e53ddd49fe59a5fa31c7443c6a79
Fabio Forni <fabio@redaril.me>
2020-01-04 11:52:43 +0100

    Refactor orders.py

--- pykamino/features/orders.py
+++ pykamino/features/orders.py
@@ -1,217 +1,240 @@
+from datetime import datetime
+from functools import partial
+from typing import List, Tuple
 import itertools
 import multiprocessing
-from statistics import mean
-
-import pandas as pd
 
 from pykamino.db import OrderState
 from pykamino.features import TimeWindow, sliding_time_windows
 from pykamino.features.decorators import rounded
+import numpy
+import pandas
 
-# FIXME: pykamino.features.trades is a function-based module
-# whereas this one is class-based. Maybe it's a better idea to unify
-# the two paradigms.
+FEATURES = ('mid_market_price', 'best_ask_price', 'best_bid_price', 'best_ask_amount',
+            'best_bid_amount', 'bid_ask_spread', 'ask_depth', 'bid_depth', 'ask_volume',
+            'bid_volume', 'ask_volume_weighted', 'bid_volume_weighted', 'sampled_chart')
 
-def memoize(func):
+
+def asks(orders: pandas.DataFrame) -> pandas.DataFrame:
     """
-    Cache the output of the function. Very useful to speed up multiple calls of
-    computational intensive functions.
+    Get orders of type "ask".
 
-    Defines, if not present, an instance variable called _cache.
+    Args:
+        orders: dataFrame of orders
     """
-    def wrapper(self, *args, **kwargs):
-        if not hasattr(self, '_cache'):
-            self._cache = {}
-        if func not in self._cache:
-            self._cache[func] = func(self, *args, **kwargs)
-        return self._cache[func]
-    return wrapper
+    return orders[orders.side == 'ask']
 
-class FeatureCalculator():
-    def __init__(self, orders, timestamp):
-        self._orders = self._open_orders_at_timestamp(orders, timestamp)
-        self.timestamp = timestamp
 
-    def _open_orders_at_timestamp(self, orders, timestamp):
-        if len(orders) == 0:
-            raise ValueError(f'No open orders at timestamp {timestamp}.')
-        condition = (
-            (orders.starting_at <= timestamp) &
-            ((orders.ending_at > timestamp) | orders.ending_at.isnull()))
-        open_orders = orders[condition]
-        # Some pandas functions, like groupby(), drop fields that are "Objects"
-        # (i.e. custom fields, Decimals...). Because of this, we pre-cast price
-        # and amount to floats.
-        return open_orders.astype({'price': float, 'amount': float})
+def bids(orders: pandas.DataFrame) -> pandas.DataFrame:
+    """
+    Get orders of type "bid".
 
-    @memoize
-    def asks(self):
-        """Ask orders sorted by price."""
-        ask_orders = self._orders[self._orders.side == 'ask']
-        if ask_orders.empty:
-            raise ValueError('No ask orders in the dataframe.')
-        return ask_orders
+    Args:
+        orders: dataFrame of orders
+    """
+    return orders[orders.side == 'bid']
 
-    @memoize
-    def bids(self):
-        """Bid orders sorted by price."""
-        bid_orders = self._orders[self._orders.side == 'bid']
-        if bid_orders.empty:
-            raise ValueError('No bid orders in the dataframe.')
-        return bid_orders
 
-    @memoize
-    def best_ask_order(self):
-        """
-        Ask order with the minimum price.
-        If there are more orders with the same price, the one with the maximum
-        amount is returned.
-        """
-        return (
-            self
-            .asks()
-            .sort_values(["price", "amount"], ascending=[True, False])
-            .iloc[0])
+def best_ask_order(orders: pandas.DataFrame) -> pandas.Series:
+    """
+    Get the ask order with the minimum price.
+    If there are more orders with the same price, the one with the maximum
+    amount is returned.
 
-    @memoize
-    def best_bid_order(self):
-        """
-        Bid order with the maximum price.
-        If there are more orders with the same price, the one with the minimum
-        amount is returned.
-        """
-        return (
-            self
-            .bids()
-            .sort_values(["price", "amount"], ascending=[True, False])
-            .iloc[-1])
+    Args:
+        orders: dataFrame of orders
+    """
+    # DataFrames are mutable, thus not hashable. For this reason we cannot make use
+    # of memoization but resort to such a hacky and stupid local-scoped cache.
+    sks = asks(orders)
+    index = sks[sks.price == sks.price.min()]['amount'].idxmax()
+    return sks.loc[index]
 
-    def best_ask_price(self):
-        """Minimum price among ask orders."""
-        return self.best_ask_order().price
 
-    def best_bid_price(self):
-        """Maximum price among bid orders."""
-        return self.best_bid_order().price
+def best_bid_order(orders: pandas.DataFrame) -> pandas.Series:
+    """
+    Get the bid order with the maximum price.
+    If there are more orders with the same price, the one with the minimum
+    amount is returned.
 
-    def best_ask_amount(self):
-        """
-        Total amount of assets of the ask orders at the best price.
-        The best ask price is the minimum price sellers are willing to accept.
-        """
-        best_price_mask = self.asks().price == self.best_ask_price()
-        return self.asks()[best_price_mask].sum().amount
+    Args:
+        orders: dataFrame of orders
+    """
+    bds = bids(orders)
+    index = bds[bds.price == bds.price.max()]['amount'].idxmin()
+    return bds.loc[index]
 
-    def best_bid_amount(self):
-        """
-        Total amount of assets of the bid orders at the best price.
-        The best bid price is the maximum price buyers are willing to pay.
-        """
-        best_price_mask = self.bids().price == self.best_bid_price()
-        return self.bids()[best_price_mask].sum().amount
 
-    @memoize
-    @rounded
-    def mid_market_price(self):
-        """
-        Mean between the best bid price and the best ask price.
-        The mid market price represents an accurate estimate of the true price
-        of the asset (BTC in this case) at one instant.
-        """
-        return mean([self.best_bid_price(), self.best_ask_price()])
+def best_ask_price(orders: pandas.DataFrame):
+    """
+    Get the minimum price among ask orders.
 
-    @rounded
-    def bid_ask_spread(self):
-        """
-        Difference between the highest price that a buyer is willing to pay
-        (bid) and the lowest price that a seller is willing to accept (ask).
-        Small spreads generate a frictionless market, where trades can occur
-        with no significant movement of the price.
-        """
-        return self.best_bid_price() - self.best_ask_price()
+    Args:
+        orders: dataFrame of orders
+    """
+    return best_ask_order(orders).price
 
-    @memoize
-    def ask_depth(self):
-        """Number of ask orders."""
-        return len(self.asks())
 
-    @memoize
-    def bid_depth(self):
-        """Number of bid orders."""
-        return len(self.bids())
+def best_bid_price(orders: pandas.DataFrame):
+    """
+    Get the maximum price among ask orders.
 
-    @memoize
-    def chart(self):
-        chart = pd.concat([self._bids_chart(), self._asks_chart()])
-        no_outliners_filter = (
-            (chart.price < 1.99 * self.mid_market_price()) &
-            (chart.price > 0.01 * self.mid_market_price()))
-        return chart[no_outliners_filter]
+    Args:
+        orders: dataFrame of orders
+    """
+    return best_bid_order(orders).price
 
-    def sampled_chart(self, bins=30):
-        return (
-            self.chart()
-            .groupby(pd.cut(self.chart().price, bins), sort=False)
+
+def best_ask_amount(orders: pandas.DataFrame):
+    """
+    Ge the total amount of assets for the ask orders at the best price.
+    The best ask price is the minimum price that sellers are willing to take.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    sks = asks(orders)
+    best_price_mask = sks.price == best_ask_price(orders)
+    return sks[best_price_mask].sum().amount
+
+
+def best_bid_amount(orders: pandas.DataFrame):
+    """
+    Get the total amount of assets for the bid orders at the best price.
+    The best bid price is the maximum price buyers are willing to pay.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    bds = bids(orders)
+    best_price_mask = bds.price == best_bid_price(orders)
+    return bds[best_price_mask].sum().amount
+
+
+@rounded
+def mid_market_price(orders: pandas.DataFrame):
+    """
+    Get the mean between the best bid price and the best ask price.
+    The mid market price represents an accurate estimate of the true price
+    of the asset (BTC, or ETH, etc.) at a given instant.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return numpy.mean((best_bid_price(orders), best_ask_price(orders)))
+
+
+@rounded
+def bid_ask_spread(orders: pandas.DataFrame):
+    """
+    Get the difference between the highest price that a buyer is willing to pay
+    (bid) and the lowest price that a seller is willing to accept (ask).
+    Small spreads generate a frictionless market, in which trades can occur
+    with no significant movement of the price.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return best_bid_price(orders) - best_ask_price(orders)
+
+
+def ask_depth(orders: pandas.DataFrame) -> int:
+    """
+    Get the number of ask orders.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return len(asks(orders))
+
+
+def bid_depth(orders: pandas.DataFrame) -> int:
+    """
+    Get the number of bid orders.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return len(bids(orders))
+
+
+def chart(orders):
+    chart = pandas.concat([bids_chart(orders), asks_chart(orders)])
+    no_outliners_filter = (
+        (chart.price < 1.99 * mid_market_price(orders)) &
+        (chart.price > 0.01 * mid_market_price(orders)))
+    return chart[no_outliners_filter]
+
+
+def sampled_chart(orders, bins=30):
+    return (chart(orders)
+            .groupby(pandas.cut(chart(orders).price, bins), sort=False)
             .mean().amount
             .tolist())
 
-    @memoize
-    def _bids_chart(self):
-        return (
-            self.bids()
-            .groupby("price")
-            .sum().amount
-            .iloc[::-1]
-            .cumsum()
-            .iloc[::-1]
-            .reset_index())
 
-    @memoize
-    def _asks_chart(self):
-        return (
-            self.asks()
-            .groupby("price")
-            .sum().amount
-            .cumsum().reset_index())
-
-    def _volume(self, orders):
-        return orders.amount.sum()
-
-    def ask_volume(self):
-        return self._volume(self.asks())
-
-    def bid_volume(self):
-        return self._volume(self.bids())
-
-    @memoize
-    @rounded
-    def ask_volume_weighted(self):
-        mmp = self.mid_market_price()
-        return self.asks().amount.dot(self.asks().price.subtract(mmp).rdiv(1))
-
-    @memoize
-    @rounded
-    def bid_volume_weighted(self):
-        mmp = self.mid_market_price()
-        return self.bids().amount.dot(self.bids().price.subtract(mmp).rdiv(-1))
-
-    def compute_all(self):
-        """Dictionary of all the features in this order book"""
-        all_features = ["mid_market_price", "best_ask_price", "best_bid_price",
-                        "best_ask_amount", "best_bid_amount",
-                        "bid_ask_spread", "ask_depth", "bid_depth",
-                        "ask_volume", "bid_volume", "ask_volume_weighted",
-                        "bid_volume_weighted", "sampled_chart"]
-        output = {}
-        for feature_name in all_features:
-            output[feature_name] = getattr(self, feature_name)()
-        return output
+def bids_chart(orders):
+    return (
+        bids(orders)
+        .groupby('price')
+        .sum().amount
+        .iloc[::-1]
+        .cumsum()
+        .iloc[::-1]
+        .reset_index())
 
 
-def fetch_states(interval, product='BTC-USD'):
+def asks_chart(orders):
+    return (
+        asks(orders)
+        .groupby('price')
+        .sum().amount
+        .cumsum().reset_index())
+
+
+def ask_volume(states: pandas.DataFrame):
     """
-    Fetch order states that are closed, or still open in the order book,
-    within the specified interval.
+    Get the total amount of assets for "ask" orders.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return asks(states).amount.sum()
+
+
+def bid_volume(states: pandas.DataFrame):
+    """
+    Get the total amount of assets for "bid" orders.
+
+    Args:
+        orders: dataFrame of orders
+    """
+    return bids(states).amount.sum()
+
+
+@rounded
+def ask_volume_weighted(states: pandas.DataFrame):
+    sks = asks(states)
+    return sks.amount.dot(sks.price.subtract(mid_market_price(states)).rdiv(1))
+
+
+@rounded
+def bid_volume_weighted(states: pandas.DataFrame):
+    bds = bids(states)
+    return bds.amount.dot(bds.price.subtract(mid_market_price(states)).rdiv(-1))
+
+
+def fetch_states(interval: TimeWindow, product: str = 'BTC-USD') -> pandas.DataFrame:
+    """
+    Get a pandas.DataFrame of all the order states in the time window. Only the open states
+    will be fetched.
+
+    Args:
+        interval: time window from which to fetch states
+        product: currency to consider
+
+    Returns:
+        order states in `interval`
     """
     orders = (
         OrderState
@@ -224,34 +247,44 @@ def fetch_states(interval, product='BTC-USD'):
             ((OrderState.ending_at > interval.start) |
              (OrderState.ending_at.is_null())))
         .namedtuples())
-    return pd.DataFrame(orders)
+    return pandas.DataFrame(orders, dtype=numpy.float64)
 
 
-def extract(interval: TimeWindow, res='2min', products=('BTC-USD',)):
+def get_open_orders(order_states: pandas.DataFrame, instant: datetime):
+    open_condition = (
+        (order_states.starting_at <= instant) &
+        ((order_states.ending_at > instant) | order_states.ending_at.isnull()))
+    return order_states[open_condition]
+
+
+def extraction_worker(intervals: List[TimeWindow], product: str = 'BTC-USD'):
+    def compute(orders, instant):
+        """
+        Take a big dataframe and compute features only for a certain time interval.
+        """
+        open_orders = get_open_orders(orders, instant)
+        feats = {'timestamp': instant}
+        module_scope = globals()
+        for f in FEATURES:
+            feats[f] = module_scope[f](open_orders)
+        return feats
+
+    range = TimeWindow(intervals[0].start, intervals[-1].end)
+    orders = fetch_states(range, product=product)
+    instants = itertools.chain(
+        (i.start for i in intervals), (intervals[-1].end,))
+    return [compute(orders, i) for i in instants]
+
+
+def extract(interval: TimeWindow, res: str = '2min', products: Tuple[str, ...] = ('BTC-USD',)):
     features = {}
-    res = pd.to_timedelta(res)
+    res = pandas.to_timedelta(res)
     with multiprocessing.Pool() as pool:
         for product in products:
-            windows = sliding_time_windows(interval, res, stride=100,
-                                           chunksize=10000)
+            windows = sliding_time_windows(interval, res, stride=100)
             # TODO: both `chunksize`s should be dynamic, depending
             # on the interval length and number of rows in the database.
-            output = pool.imap(_extract, windows, chunksize=2)
+            worker = partial(extraction_worker, product)
+            output = pool.imap(worker, windows, chunksize=2)
             features[product] = list(itertools.chain(*output))
-    # TODO: Support multiple currencies. For now we consider only BTC
-    return features['BTC-USD']
-
-
-def _extract(intervals):
-    range = TimeWindow(intervals[0].start, intervals[-1].end)
-    orders = fetch_states(range)
-    instants = [i.start for i in intervals]
-    instants.append(intervals[-1].end)
-    return [features_from_subset(orders, i) for i in instants]
-
-
-def features_from_subset(orders, instant):
-    if len(orders) == 0:
-        return None
-    fc = FeatureCalculator(orders, instant)
-    return {**fc.compute_all(), 'timestamp': instant}
+    return features
